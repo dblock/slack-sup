0c62893a0818255f64147bf4498fa489f661e277
dblock <dblock@dblock.org>
2017-08-29 12:55:31 -0400

    Fix: wrong minutes shown. Closes #35.

--- spec/integration/gcal_spec.rb
+++ spec/integration/gcal_spec.rb
@@ -6,6 +6,6 @@ describe 'GCal', js: true, type: :feature do
   let(:monday) { DateTime.parse('2017/1/2 8:00 AM EST').utc }
   it 'creates a calendar event' do
     visit "/gcal?sup_id=#{sup.id}&dt=1483394400"
-    expect(find('#messages')).to have_text("Adding S'Up calendar for on Monday, January 02, 2017 at 5:01 pm ...")
+    expect(find('#messages')).to have_text("Adding S'Up calendar for on Monday, January 02, 2017 at 5:00 pm ...")
   end
 end
