bf9de01b2ca28b32814166a56ba6100759563c54
dblock <dblock@dblock.org>
2017-09-04 22:22:10 -0400

    Disable GCal specs in CI.

--- spec/integration/gcal_spec.rb
+++ spec/integration/gcal_spec.rb
@@ -1,47 +1,49 @@
 require 'spec_helper'
 
-describe 'GCal', js: true, type: :feature do
-  it 'errors without GOOGLE_API_CLIENT_ID' do
-    visit '/gcal?1'
-    expect(find('#messages')).to have_text('Missing GOOGLE_API_CLIENT_ID.')
-  end
-  context 'with GOOGLE_API_CLIENT_ID' do
-    before do
-      ENV['GOOGLE_API_CLIENT_ID'] = 'client-id'
+unless ENV['CI']
+  describe 'GCal', js: true, type: :feature do
+    it 'errors without GOOGLE_API_CLIENT_ID' do
+      visit '/gcal?1'
+      expect(find('#messages')).to have_text('Missing GOOGLE_API_CLIENT_ID.')
     end
-    after do
-      ENV.delete('GOOGLE_API_CLIENT_ID')
-    end
-    it 'errors without any parameters' do
-      visit '/gcal?2'
-      expect(find('#messages', visible: true)).to have_text("Missing or invalid S'Up ID.")
-    end
-    it 'errors without a sup ID' do
-      visit '/gcal?dt=1483394400'
-      expect(find('#messages', visible: true)).to have_text("Missing or invalid S'Up ID.")
-    end
-    context 'with a team' do
-      let(:team) { Fabricate(:team) }
-      let!(:sup) { Fabricate(:sup, team: team, channel_id: 'sup-channel-id') }
-      let(:monday) { DateTime.parse('2017/1/2 8:00 AM EST').utc }
-      it 'errors without a sup time' do
-        visit "/gcal?sup_id=#{sup.id}"
-        expect(find('#messages', visible: true)).to have_text("Missing or invalid S'Up time.")
+    context 'with GOOGLE_API_CLIENT_ID' do
+      before do
+        ENV['GOOGLE_API_CLIENT_ID'] = 'client-id'
       end
-      it 'errors without an access token' do
-        visit "/gcal?sup_id=#{sup.id}&dt=1483394400"
-        expect(find('#messages', visible: true)).to have_text("This link has expired, ask for a new one on your S'Up channel.")
+      after do
+        ENV.delete('GOOGLE_API_CLIENT_ID')
       end
-      it 'errors with an expired access token' do
-        access_token = team.short_lived_token
-        Timecop.travel(30.minutes.from_now)
-        visit "/gcal?sup_id=#{sup.id}&dt=1483394400&access_token=#{access_token}"
-        expect(find('#messages', visible: true)).to have_text("This link has expired, ask for a new one on your S'Up channel.")
+      it 'errors without any parameters' do
+        visit '/gcal?2'
+        expect(find('#messages', visible: true)).to have_text("Missing or invalid S'Up ID.")
       end
-      it 'creates a calendar event' do
-        # Firefox may fail locally with idpiframe_initialization_failed
-        visit "/gcal?sup_id=#{sup.id}&dt=1483394400&access_token=#{sup.team.short_lived_token}"
-        expect(find('#messages', visible: true)).to have_text("Adding S'Up calendar for on Monday, January 02, 2017 at 5:00 pm ...")
+      it 'errors without a sup ID' do
+        visit '/gcal?dt=1483394400'
+        expect(find('#messages', visible: true)).to have_text("Missing or invalid S'Up ID.")
+      end
+      context 'with a team' do
+        let(:team) { Fabricate(:team) }
+        let!(:sup) { Fabricate(:sup, team: team, channel_id: 'sup-channel-id') }
+        let(:monday) { DateTime.parse('2017/1/2 8:00 AM EST').utc }
+        it 'errors without a sup time' do
+          visit "/gcal?sup_id=#{sup.id}"
+          expect(find('#messages', visible: true)).to have_text("Missing or invalid S'Up time.")
+        end
+        it 'errors without an access token' do
+          visit "/gcal?sup_id=#{sup.id}&dt=1483394400"
+          expect(find('#messages', visible: true)).to have_text("This link has expired, ask for a new one on your S'Up channel.")
+        end
+        it 'errors with an expired access token' do
+          access_token = team.short_lived_token
+          Timecop.travel(30.minutes.from_now)
+          visit "/gcal?sup_id=#{sup.id}&dt=1483394400&access_token=#{access_token}"
+          expect(find('#messages', visible: true)).to have_text("This link has expired, ask for a new one on your S'Up channel.")
+        end
+        it 'creates a calendar event' do
+          # Firefox may fail locally with idpiframe_initialization_failed
+          visit "/gcal?sup_id=#{sup.id}&dt=1483394400&access_token=#{sup.team.short_lived_token}"
+          expect(find('#messages', visible: true)).to have_text("Adding S'Up calendar for on Monday, January 02, 2017 at 5:00 pm ...")
+        end
       end
     end
   end
