0103084aad385a7d1b6580fc7e71764387ba6c3e
dblock <dblock@dblock.org>
2020-07-05 21:30:56 -0400

    Use distance_of_time_in_words.

--- spec/slack-sup/commands/stats_spec.rb
+++ spec/slack-sup/commands/stats_spec.rb
@@ -7,7 +7,7 @@ describe SlackSup::Commands::Stats do
   it 'empty stats' do
     expect(message: "#{SlackRubyBot.config.user} stats").to respond_with_slack_message(
       "Team S'Up connects groups of 3 people on Monday after 9:00 AM every week.\n" \
-      "Team S'Up started 21 days ago with no users (0/0) opted in."
+      "Team S'Up started 3 weeks ago with no users (0/0) opted in."
     )
   end
   context 'with outcomes' do
@@ -29,7 +29,7 @@ describe SlackSup::Commands::Stats do
     it 'reports counts' do
       expect(message: "#{SlackRubyBot.config.user} stats").to respond_with_slack_message(
         "Team S'Up connects groups of 3 people on Monday after 9:00 AM every week.\n" \
-        "Team S'Up started 21 days ago with 66% (2/3) of users opted in.\n" \
+        "Team S'Up started 3 weeks ago with 66% (2/3) of users opted in.\n" \
         "Facilitated 2 S'Ups in 2 rounds for 3 users with 50% positive outcomes from 50% outcomes reported."
       )
     end
