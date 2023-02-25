4e4b7b51248257072bc5a77a064417198f2c92d8
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2023-02-25 16:00:54 -0500

    Add DM specs for gcal.

--- spec/slack-sup/commands/gcal_spec.rb
+++ spec/slack-sup/commands/gcal_spec.rb
@@ -1,19 +1,38 @@
 require 'spec_helper'
 
 describe SlackSup::Commands::GCal do
-  let!(:team) { Fabricate(:team) }
-  let!(:channel) { Fabricate(:channel, team: team) }
-  let!(:user) { Fabricate(:user, channel: channel) }
-  let(:app) { SlackSup::Server.new(team: team) }
-  let(:client) { app.send(:client) }
-  context 'gcal' do
-    it 'requires a subscription' do
-      expect(message: "#{SlackRubyBot.config.user} gcal", user: user.user_id).to respond_with_slack_message(team.subscribe_text)
+  include_context :client
+
+  context 'DM' do
+    include_context :team
+
+    before do
+      ENV['GOOGLE_API_CLIENT_ID'] = 'client-id'
     end
+    after do
+      ENV.delete('GOOGLE_API_CLIENT_ID')
+    end
+
+    it 'requires a sup' do
+      expect(message: "#{SlackRubyBot.config.user} gcal").to respond_with_slack_message(
+        "Please `@sup cal date/time` inside a S'Up DM channel."
+      )
+    end
+  end
+
+  context 'team' do
+    let!(:team) { Fabricate(:team) }
+    it 'requires a subscription' do
+      expect(message: "#{SlackRubyBot.config.user} gcal").to respond_with_slack_message(team.subscribe_text)
+    end
+  end
+
+  context 'channel' do
+    include_context :channel
+
     context 'subscribed team' do
-      let(:team) { Fabricate(:team, subscribed: true) }
       it 'requires a GOOGLE_API_CLIENT_ID' do
-        expect(message: "#{SlackRubyBot.config.user} gcal", user: user.user_id).to respond_with_slack_message(
+        expect(message: "#{SlackRubyBot.config.user} gcal").to respond_with_slack_message(
           'Missing GOOGLE_API_CLIENT_ID.'
         )
       end
@@ -26,23 +45,23 @@ describe SlackSup::Commands::GCal do
         end
         context 'outside of a sup' do
           it 'requires a sup DM' do
-            expect(message: "#{SlackRubyBot.config.user} gcal", user: user.user_id).to respond_with_slack_message(
+            expect(message: "#{SlackRubyBot.config.user} gcal", channel: 'invalid').to respond_with_slack_message(
               "Please `@sup cal date/time` inside a S'Up DM channel."
             )
           end
         end
         context 'inside a sup' do
-          let!(:sup) { Fabricate(:sup, users: [user], channel: channel, conversation_id: 'sup-channel-id') }
+          let!(:sup) { Fabricate(:sup, channel: channel, conversation_id: 'sup-channel-id') }
           let(:monday) { DateTime.parse('2017/1/2 8:00 AM EST').utc }
           it 'requires a date/time' do
-            expect(message: "#{SlackRubyBot.config.user} gcal", user: user.user_id, channel: 'sup-channel-id').to respond_with_slack_message(
+            expect(message: "#{SlackRubyBot.config.user} gcal", channel: 'sup-channel-id').to respond_with_slack_message(
               'Please specify a date/time, eg. `@sup cal tomorrow 5pm`.'
             )
           end
           it 'creates a link' do
             Timecop.travel(monday).freeze do
               Chronic.time_class = channel.sup_tzone
-              expect(message: "#{SlackRubyBot.config.user} gcal today 5pm", user: user.user_id, channel: 'sup-channel-id').to respond_with_slack_message(
+              expect(message: "#{SlackRubyBot.config.user} gcal today 5pm", channel: 'sup-channel-id').to respond_with_slack_message(
                 "Click this link to create a gcal for Monday, January 02, 2017 at 5:00 pm: https://sup.playplay.io/gcal?sup_id=#{sup.id}&dt=1483394400&access_token=#{team.short_lived_token}"
               )
             end
