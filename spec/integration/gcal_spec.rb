90affe4a3a14f692c0b40c63fcf9811e769b4131
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2023-03-04 15:34:47 -0500

    Don't create calendars with no emails.

--- spec/integration/gcal_spec.rb
+++ spec/integration/gcal_spec.rb
@@ -39,11 +39,34 @@ unless ENV['CI']
           visit "/gcal?sup_id=#{sup.id}&dt=1483394400&access_token=#{access_token}"
           expect(find('#messages', visible: true)).to have_text("This link has expired, ask for a new one on your S'Up channel.")
         end
-        unless ENV['CI']
-          it 'creates a calendar event' do
-            # Firefox will fail locally with idpiframe_initialization_failed
-            visit "/gcal?sup_id=#{sup.id}&dt=1483394400&access_token=#{sup.channel.short_lived_token}"
-            expect(find('#messages', visible: true)).to have_text("Adding S'Up calendar for on Monday, January 02, 2017 at 5:00 pm ...")
+        it 'errors when users do not have any users' do
+          visit "/gcal?sup_id=#{sup.id}&dt=1483394400&access_token=#{channel.short_lived_token}"
+          expect(find('#messages', visible: true)).to have_text("S'Up does not have any users.")
+        end
+        context 'with users' do
+          before do
+            3.times do
+              sup.users << Fabricate(:user, channel: channel)
+            end
+          end
+          it 'errors when users do not have e-mail addresses' do
+            visit "/gcal?sup_id=#{sup.id}&dt=1483394400&access_token=#{channel.short_lived_token}"
+            expect(find('#messages', visible: true)).to have_text('None of 3 users have e-mail addresses.')
+          end
+          it 'errors when some of the users do not have e-mail addresses' do
+            sup.users.first.update_attributes!(email: Faker::Internet.email)
+            visit "/gcal?sup_id=#{sup.id}&dt=1483394400&access_token=#{channel.short_lived_token}"
+            expect(find('#messages', visible: true)).to have_text('Only 1 of 3 users have e-mail addresses.')
+          end
+          unless ENV['CI']
+            it 'creates a calendar event' do
+              sup.users.each do |user|
+                user.update_attributes!(email: Faker::Internet.email)
+              end
+              # Firefox will fail locally with idpiframe_initialization_failed
+              visit "/gcal?sup_id=#{sup.id}&dt=1483394400&access_token=#{sup.channel.short_lived_token}"
+              expect(find('#messages', visible: true)).to have_text("Adding S'Up calendar for on Monday, January 02, 2017 at 5:00 pm ...")
+            end
           end
         end
       end
