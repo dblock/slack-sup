d5575be63029535c40a354f2d4d04df726092006
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2024-07-27 18:48:17 -0400

    Moved function declarations before document load, fixes reporting gcal errors.

--- public/js/sup.js
+++ public/js/sup.js
@@ -1,42 +1,39 @@
 var SlackSup = {};
 
-$(document).ready(function() {
+SlackSup.message = function(text) {
+  $('#messages').removeClass('has-error');
+  $('#messages').fadeOut('slow', function() {
+    $('#messages').fadeIn('slow').html(text)
+  });
+};
 
-  SlackSup.message = function(text) {
-    $('#messages').removeClass('has-error');
-    $('#messages').fadeOut('slow', function() {
-      $('#messages').fadeIn('slow').html(text)
-    });
-  };
+SlackSup.register = function(text) {
+  $('.navbar').fadeOut('slow');
+  $('header').fadeOut('slow');
+  $('section').fadeOut('slow');
+  $('#register').show();
+};
 
-  SlackSup.register = function(text) {
-    $('.navbar').fadeOut('slow');
-    $('header').fadeOut('slow');
-    $('section').fadeOut('slow');
-    $('#register').show();
-  };
+SlackSup.errorMessage = function(message) {
+  SlackSup.message(message)
+  $('#messages').addClass('has-error');
+};
 
-  SlackSup.errorMessage = function(message) {
-    SlackSup.message(message)
-    $('#messages').addClass('has-error');
-  };
-
-  SlackSup.error = function(xhr) {
-    var message;
-    if (xhr.responseText) {
-      var rc = JSON.parse(xhr.responseText);
-      if (rc && rc.error) {
-        message = rc.error;
-      } else if (rc && rc.message) {
-        message = rc.message;
-        if (message == 'invalid_code') {
-          message = 'The code returned from the OAuth workflow was invalid.'
-        } else if (message == 'code_already_used') {
-          message = 'The code returned from the OAuth workflow has already been used.'
-        }
+SlackSup.error = function(xhr) {
+  var message;
+  if (xhr.responseText) {
+    var rc = JSON.parse(xhr.responseText);
+    if (rc && rc.error) {
+      message = rc.error;
+    } else if (rc && rc.message) {
+      message = rc.message;
+      if (message == 'invalid_code') {
+        message = 'The code returned from the OAuth workflow was invalid.'
+      } else if (message == 'code_already_used') {
+        message = 'The code returned from the OAuth workflow has already been used.'
       }
     }
+  }
 
-    SlackSup.errorMessage(message || xhr.statusText || xhr.responseText || 'Unexpected Error');
-  };
-});
+  SlackSup.errorMessage(message || xhr.statusText || xhr.responseText || 'Unexpected Error');
+};
